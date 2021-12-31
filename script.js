b4484da807f55c149330e56eddc6e0ca72c6b291
Asim Sapkota <asimsapkota1836@gmail.com>
2022-01-01 02:04:22 +0545

    initial commit with merge sort.

---
+++ script.js
@@ -0,0 +1,53 @@
+function divideAry(ary) {
+  let container = [];
+  for (value of ary) {
+    container.push([value]);
+  }
+  return container;
+}
+
+let test = divideAry([38, 27, 43, 3, 9, 82, 10]);
+
+function mergeAry(ary) {
+  let nextAry = [];
+  if (ary.length < 2) {
+    return ary;
+  }
+  for (let i = 0; i < ary.length; i += 2) {
+    if (ary.length % 2 > 0 && i >= ary.length - 1) {
+      nextAry.push([...ary[i]]);
+      continue;
+    }
+    nextAry.push(intermediateSort(ary[i], ary[i + 1]));
+  }
+  return mergeAry(nextAry);
+}
+
+// console.log(mergeAry(test));
+
+function intermediateSort([...ary0], [...ary1]) {
+  let tmpAry = [];
+  while (ary0.length > 0) {
+    if (ary1.length < 1) {
+      tmpAry.push(...ary0);
+      break;
+    }
+    if (ary0[0] > ary1[0]) {
+      tmpAry.push(ary1[0]);
+      ary1.shift();
+      continue;
+    }
+    tmpAry.push(ary0[0]);
+    ary0.shift();
+  }
+  tmpAry.push(...ary1);
+  return tmpAry;
+}
+
+// console.log(intermediateSort([1], [1, 2]));
+
+function mergeSort(ary) {
+  return mergeAry(divideAry(ary))[0];
+}
+
+console.log(mergeSort([38, 27, 43, 3, 9, 82, 10]));
